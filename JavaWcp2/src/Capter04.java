25f01c4b71c164a007e9ea0fd090cc6794dc6d44
すみれ荘 <sumireso@sumiresounoMacBook-Pro.local>
2022-12-01 23:51:12 +0900

    [Start]java_studing

---
+++ JavaWcp2/src/Capter04.java
@@ -0,0 +1,13 @@
+
+public class Capter04 {
+	public static void main(String[] args) {
+		String str1 = "Hayahiko Shiina";
+		System.out.println(str1);
+
+		str1 = "Program";
+		System.out.println(str1);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
