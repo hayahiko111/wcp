a6236a3dc1ddeee0b4e6e9a1f8a33196c15be9c5
すみれ荘 <sumireso@sumiresounoMacBook-Pro.local>
2022-12-03 00:57:42 +0900

    [Practice]B-5

---
+++ JavaWcp2/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1L;
+		double double1 = 0.01;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+
+		Float Float1 = 0.1F;
+		System.out.println(Float1);
+		Integer Integer1 = 1;
+		System.out.println(Integer1);
+		Character Character1 = 'a';
+		System.out.println(Character1);
+
+		int[] array1 = { 1, 2, 3, 4, 5 };
+		System.out.println(array1[3]);
+	}
+}
